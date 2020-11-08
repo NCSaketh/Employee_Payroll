822f50922940b89d99518d99fa9ac82d8b93ae60
NCSaketh <nc.saketh1@gmail.com>
2020-11-08 23:54:26 +0530

    UC2: Retreiving data from database

--- src/main/java/com/cg/employee_payroll/PayrollService.java
+++ src/main/java/com/cg/employee_payroll/PayrollService.java
@@ -3,9 +3,13 @@ package com.cg.employee_payroll;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.*;
+import java.util.HashMap;
 import java.util.Properties;
 
 public class PayrollService {
+
+    HashMap<Integer, EmployeePayroll> employeePayrollMap = new HashMap<>();
+
     private static Connection getConnection() throws IOException, SQLException {
         FileInputStream fin = new FileInputStream("C:\\Users\\Nc Saketh\\intellij-workspace\\Employee_Payroll\\config.properties");
         Properties prop = new Properties();
@@ -29,6 +33,29 @@ public class PayrollService {
             }
         } catch (IOException | SQLException e) {
             e.printStackTrace();
+        }
+    }
+
+
+    public void getRecordsFromDB(PayrollService payrollService, Statement stmt) throws SQLException {
+        String query = "select * from employee e, payroll p " + "where e.emp_id = p.emp_id";
+        ResultSet resultSet = stmt.executeQuery(query);
+        while (resultSet.next()) {
+            EmployeePayroll tempLoopObject = new EmployeePayroll();
+            tempLoopObject.company_id = resultSet.getInt("company_id");
+            tempLoopObject.emp_id = resultSet.getInt("emp_id");
+            tempLoopObject.name = resultSet.getString("name");
+            tempLoopObject.phone = resultSet.getString("phone");
+            tempLoopObject.address = resultSet.getString("address");
+            tempLoopObject.gender = resultSet.getString("gender").charAt(0);
+            tempLoopObject.start = resultSet.getDate("start");
+            tempLoopObject.basic_pay = resultSet.getDouble("basic_pay");
+            tempLoopObject.deductions = resultSet.getDouble("deductions");
+            tempLoopObject.taxable_pay = resultSet.getDouble("taxable_pay");
+            tempLoopObject.tax = resultSet.getDouble("tax");
+            tempLoopObject.net_pay = resultSet.getDouble("net_pay");
+            payrollService.employeePayrollMap.put(tempLoopObject.emp_id, tempLoopObject);
+            System.out.println(tempLoopObject);
         }
     }
 }
